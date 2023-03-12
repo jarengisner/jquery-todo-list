dfb3b9732e878c8a208f9d1ba338820b385e2898
jarengisner <124743127+jarengisner@users.noreply.github.com>
2023-03-12 12:22:29 -0700

    Wrote out basic jQuery for project
    This code employs all functionality for the to-do list.

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+function newItem() {
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert('You must write something');
+  } else {
+    $('#list').append(li);
+  }
+
+  function crossOut() {
+    li.toggleClass('strike');
+  }
+
+  li.on('dblclick', function () {
+    li.toggleClass('strike');
+  });
+
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  crossOutButton.on('click', function () {
+    deleteListItem();
+  });
+
+  function deleteListItem() {
+    li.addClass('delete');
+  }
+  //This library is so cool!//
+  $('#list').sortable();
+}
